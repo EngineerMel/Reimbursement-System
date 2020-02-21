ebca8946682e723121c73bbc4f902c8174243012
EngineerMel <ramelhaines25@gmail.com>
2020-02-21 12:41:21 -0500

    creating endpoint template

--- src/index.ts
+++ src/index.ts
@@ -10,5 +10,21 @@ app.get("/api/users", (req, res) => {
   res.send([1, 2, 3]);
 });
 
+app.get("/api/users/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+//below is how we add multiple parameters
+// app.get("/api/users/:id/:role", (req, res) => {
+//   res.send(req.params.id);
+// });
+
+/* "/api/users/:id?sortBy=name" <<<this is how the endpoint would look for query string 
+paramter which is used to provide additional data to the backend services/*
+
+ app.get("/api/users/:id", (req, res) => {
+   res.send(req.query);
+ }); */
+
 const port = process.env.PORT || 2020;
 app.listen(port, () => console.log(`Listening on ${port}`));
