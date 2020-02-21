1fdd2b5b06ebf1bad38f0fea63021ec11590e05d
EngineerMel <ramelhaines25@gmail.com>
2020-02-21 13:50:37 -0500

    handling get and post request

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,41 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+const users = [
+  { id: 1, username: "Melly", password: "test1" },
+  { id: 2, username: "Melly", password: "test2" },
+  { id: 3, username: "Melly", password: "test3" },
+  { id: 4, username: "Melly", password: "test4" }
+];
+
+//testing endpoint
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+//List all users
 app.get("/api/users", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send([users]);
 });
 
+//creates a user
+app.post("/api/users", (req, res) => {
+  const user = {
+    id: users.length + 1,
+    username: req.body.username,
+    password: req.body.password
+  };
+  users.push(user);
+  res.send(user);
+});
+
+//find a user by id
 app.get("/api/users/:id", (req, res) => {
-  res.send(req.params.id);
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if (!user) res.status(404).send("The user with this given id was not found");
+  res.send(user);
 });
 
 //below is how we add multiple parameters
@@ -26,5 +51,6 @@ paramter which is used to provide additional data to the backend services/*
    res.send(req.query);
  }); */
 
+//shows what port the application runs on
 const port = process.env.PORT || 2020;
 app.listen(port, () => console.log(`Listening on ${port}`));
