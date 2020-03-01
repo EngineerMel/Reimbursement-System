09878bd2ee9399a5d89edc07c3e195c24ca21fa9
EngineerMel <ramelhaines25@gmail.com>
2020-03-01 13:34:48 -0500

    updated reimburserouters

--- src/Routers/ReimbursementRouter.ts
+++ src/Routers/ReimbursementRouter.ts
@@ -2,3 +2,40 @@ import * as express from "express";
 import { ReimbursementDTO } from "../DTOs/ReimbursementDTO";
 import { sessionMiddleware } from "../Middleware/session-middleware";
 import { authFactory } from "../Middleware/auth.middleware";
+import { submitReimbursement } from "../Services/ReimbursementService";
+
+export const reimbursementRouter = express.Router();
+reimbursementRouter.use(sessionMiddleware);
+
+//SUBMIT REIMBURSEMENT
+reimbursementRouter.post(
+  "",
+  authFactory(["Finance-Manager", "User", "Admin"]),
+  async (req, res) => {
+    try {
+      const { amount, dateSubmitted, description, type } = req.body;
+      if (amount && dateSubmitted && description && type) {
+        const reimb = await submitReimbursement(
+          new ReimbursementDTO(
+            0,
+            req.session.user.user_id,
+            amount,
+            dateSubmitted,
+            20190101,
+            description,
+            null,
+            2,
+            type
+          )
+        );
+        res.status(201).json(reimb);
+      } else {
+        throw new Error(
+          "Please provide all details to submit a reimbursement request."
+        );
+      }
+    } catch (e) {
+      res.status(e.status).send(e.message);
+    }
+  }
+);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import * as express from "express";
 import * as bodyparser from "body-parser";
 import { userRouter } from "./Routers/UserRouter";
+import { reimbursementRouter } from "./Routers/ReimbursementRouter";
 
 const app = express();
 
 app.use("/", bodyparser.json());
 app.use("/users", userRouter);
+app.use("/reimbursements", reimbursementRouter);
 
 //shows what port the application runs on
 const port = process.env.PORT || 2020;
